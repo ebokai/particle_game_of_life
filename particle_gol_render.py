e8a7dbdd717352c11eafaa649e5ee1b23a5f80af
ebokai <33095862+ebokai@users.noreply.github.com>
2021-07-02 16:13:51 +0200

    Update particle_gol_render.py

--- particle_gol_render.py
+++ particle_gol_render.py
@@ -3,8 +3,8 @@ import os
 import time
 
 import moviepy.video.io.ImageSequenceClip
-image_folder='c:/users/hp/desktop/frames'
-output_folder='c:/users/hp/desktop/frames/render'
+image_folder='c:/your_image_folder'
+output_folder='c:/your_output_folder'
 fps=24
 
 image_files = [image_folder+'/'+img for img in os.listdir(image_folder) if img.endswith(".png")]
