6738d85da59f52e16051ef312102e7f4b5e5f49b
ebokai <ebokai@hotmail.com>
2023-12-22 11:08:57 +0100

    different resolution

--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -5,8 +5,8 @@
 int main(int argc, char *argv[]){
 
 	// SCREEN SIZE =====
-	unsigned int width = 1280;
-	unsigned int height = 720;
+	unsigned int width = 1600;
+	unsigned int height = 900;
 
 	// SIMULATION =====
 	Framework framework(width, height);
