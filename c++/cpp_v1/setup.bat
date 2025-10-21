5bdec5fd2d8993e0f7b4bd5ec85232933ae541c5
ebokai <ebokai@hotmail.com>
2025-10-21 21:50:04 +0200

    Refactor directory structure for C++ projects
    Renamed 'v1' and 'v2' directories to 'cpp_v1' and 'cpp_v2' for consistency and clarity. Updated .gitignore to exclude .txt files and bin_old directory. Removed obsolete test build executables and a design document.

---
+++ c++/cpp_v1/setup.bat
@@ -0,0 +1,2 @@
+ g++ .\src\main.cpp .\src\functions.cpp .\src\framework.cpp -O3 -ffast-math -march=native -I .\src\ -I .\sdl\include\SDL2\ -L .\sdl\lib -lSDL2main -lSDL2 -o .\bin\pgol_v1.exe
+pause
--- c++/v1/setup.bat
+++
@@ -1,2 +0,0 @@
- g++ .\src\main.cpp .\src\functions.cpp .\src\framework.cpp -O3 -ffast-math -march=native -I .\src\ -I .\sdl\include\SDL2\ -L .\sdl\lib -lSDL2main -lSDL2 -o .\bin\pgol_test_build.exe
-pause
